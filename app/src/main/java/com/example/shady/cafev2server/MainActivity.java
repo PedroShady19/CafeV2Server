4cba27fc98db8917310bf03dc1d0afe165883515
Your Name <example@example.com>
2018-10-24 17:16:26 +0100

    commit 2

--- app/src/main/java/com/example/shady/cafev2server/MainActivity.java
+++ app/src/main/java/com/example/shady/cafev2server/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.shady.cafev2server;
 
+import android.graphics.Typeface;
+import android.support.constraint.solver.SolverVariable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.lang.reflect.Type;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnSignIn;
+    TextView txtSlogan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSignIn =findViewById(R.id.btnSignUp);
+        txtSlogan =findViewById(R.id.textSlogan);
+
+        Typeface face= Typeface.createFromAsset(getAssets(),"fonts/Nabila.ttf");
+        txtSlogan.setTypeface(face);
     }
 }
