0c13469e25d8f7f83fa2cef66e14be4e99982777
PedroShady19 <pmff.pedro19999@gmail.com>
2018-11-28 20:24:43 +0000

    commit 22 Mapas funciona mas Notificação nao

--- app/src/main/java/com/example/shady/cafev2server/Common/Common.java
+++ app/src/main/java/com/example/shady/cafev2server/Common/Common.java
@@ -1,9 +1,11 @@
 package com.example.shady.cafev2server.Common;
 
+import com.example.shady.cafev2server.Model.Request;
 import com.example.shady.cafev2server.Model.User;
 
 public class Common {
     public static User currentUser;
+    public static Request currentRequest;
 
     public static final String UPDATE = "Update";
     public static final String DELETE = "Delete";
